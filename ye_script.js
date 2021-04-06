c00118566992383ac00b913cc704a715c0bde891
Filip Nikolas Parak <filip.parak@yeself.com>
2021-04-06 10:09:28 +0200

    ie script

--- ye_script.js
+++ ye_script.js
@@ -4,7 +4,7 @@
   }, i[r].l = 1 * new Date(); a = s.createElement(o),
     m = s.getElementsByTagName(o)[0]; a.async = 1; a.id = 'yesElf'; a.src = g; m.parentNode.insertBefore(a, m.nextSibling)
 })
-  (window, document, 'script', 'https://cloud.yeself.com:443/sdk/298b51f5-f6f2-410b-9981-7887808dd66c/app.js', 'yesElf');
+  (window, document, 'script', 'https://cloud.yeself.com:443/sdk/298b51f5-f6f2-410b-9981-7887808dd66c/app.ie.js', 'yesElf');
 yesElf('id', '298b51f5-f6f2-410b-9981-7887808dd66c');
 yesElf('popups', true);
 yesElf('segmentation', 'userId', 'testId');
